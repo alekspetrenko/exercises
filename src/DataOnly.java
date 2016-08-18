b5e9eb3123afba665be6d85aa90bda75d2fe2899
Aleksandr Petrenko <petrenko.aleksandr025@gmail.com>
2016-08-18 13:45:37 +0300

    initial commit

---
+++ src/DataOnly.java
@@ -0,0 +1,20 @@
+
+public class DataOnly {
+	int i;
+	double d;
+	boolean b;
+
+	public static void main(String[] args) {
+		DataOnly a = new DataOnly();
+		a.i = 47;
+		a.d = 1.1;
+		a.b = false;
+		System.out.println("data.i = " + a.i);
+		System.out.print("data.d = " + a.d);
+		System.out.println("data.b = " + a.b);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
